ffa67c5ebb3e15e36616b237c869e212362c0725
Brabbins <ctltsupport@dhcp-206-87-193-161.ubcsecure.wireless.ubc.ca>
2014-09-11 10:43:56 -0700

    golbal-js.js updated, deleted img directory

---
+++ js/global-js.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function ($) {
+$(".audience-button")
+.insertAfter("#ubc7-unit .btn-navbar");
+$(".category-title")
+.insertBefore("#container");
+$("#toc")
+.insertAfter(".tableoc");
+});
