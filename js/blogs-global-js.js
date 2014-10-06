f03fc83359fce4fae79bd38a5f34aae006a52742
Brabbins <ctltsupport@dhcp-128-189-69-206.ubcsecure.wireless.ubc.ca>
2014-10-06 13:18:37 -0700

    Add smooth scroll for homepage Find Out More link

--- js/blogs-global-js.js
+++ js/blogs-global-js.js
@@ -1,8 +1,16 @@
 jQuery(document).ready(function ($) {
+//Adds audience button to menu
 $(".audience-button")
 .insertAfter("#ubc7-unit .btn-navbar");
+//Moves category title above container div to reflect website layout
 $(".category-title")
 .insertBefore("#container");
+//Moves the wiki embed table content to a side column
 $("#toc")
 .insertAfter(".tableoc");
+$(".learn-more").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".entry-content").offset().top
+	    }, 500);
+	});
 });
